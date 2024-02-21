635f9a87e407582e6c37fe3b737c35dfb7e061a3
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-02-21 16:51:23 +0700

    Инициализация проекта

---
+++ src/main/java/ru/hits/trb/trbcore/TrbCoreApplication.java
@@ -0,0 +1,13 @@
+package ru.hits.trb.trbcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrbCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrbCoreApplication.class, args);
+	}
+
+}
