0e39bb7b34aae7cef1d5c4cb00345f487bb1b0c4
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-03-02 16:48:12 +0700

    Поправил формат даты на timestamp в ответе

--- src/main/java/ru/hits/trb/trbcore/dto/account/AccountDto.java
+++ src/main/java/ru/hits/trb/trbcore/dto/account/AccountDto.java
@@ -1,5 +1,6 @@
 package ru.hits.trb.trbcore.dto.account;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import ru.hits.trb.trbcore.entity.enumeration.AccountType;
@@ -28,10 +29,18 @@ public class AccountDto {
     @Schema(description = "Внешний идентификатор пользователя из trb-users", requiredMode = REQUIRED)
     private String externalClientId;
 
-    @Schema(description = "Дата создания счета", requiredMode = REQUIRED)
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
+    @Schema(description = "Дата создания счета",
+            requiredMode = REQUIRED,
+            example = "1709372199882",
+            type = "string")
     private Date creationDate;
 
-    @Schema(description = "Дата закрытия счета", requiredMode = NOT_REQUIRED)
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
+    @Schema(description = "Дата закрытия счета",
+            requiredMode = NOT_REQUIRED,
+            example = "1709372199882",
+            type = "string")
     private Date closingDate;
 
     @Schema(description = "Признак закрытия счета", requiredMode = REQUIRED)
