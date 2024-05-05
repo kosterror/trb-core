428f24c829fc6993cda4828f4f5c1206334da433
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-05-05 19:07:55 +0700

    Поправил трассировку

--- src/main/java/ru/hits/trb/trbcore/configuration/ExchangeRateClientConfiguration.java
+++ src/main/java/ru/hits/trb/trbcore/configuration/ExchangeRateClientConfiguration.java
@@ -1,8 +1,10 @@
 package ru.hits.trb.trbcore.configuration;
 
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.observation.DefaultClientRequestObservationConvention;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -12,9 +14,11 @@ public class ExchangeRateClientConfiguration {
     private String url;
 
     @Bean
-    public RestClient exchangeRateClient() {
+    public RestClient exchangeRateClient(ObservationRegistry observationRegistry) {
         return RestClient.builder()
                 .baseUrl(url)
+                .observationRegistry(observationRegistry)
+                .observationConvention(new DefaultClientRequestObservationConvention())
                 .build();
     }
 
